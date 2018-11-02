edfb0aa4fccb5a1af7e1a487b90736980f5b3a77
esenor <daviddieu80@gmail.com>
2018-11-02 14:02:22 +0100

    Catalogue bundle

--- src/Efc/CatalogueBundle/Controller/DefaultController.php
+++ src/Efc/CatalogueBundle/Controller/DefaultController.php
@@ -6,8 +6,20 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
-    public function indexAction()
+    public function listAction()
     {
-        return $this->render('@EfcCatalogue/Default/index.html.twig');
+        return $this->render('@EfcCatalogue/Default/list.html.twig', [
+            'browserPageTitle' => 'Catalogue listing',
+            'products' => [
+                [
+                    'sku' => 'ref_0001',
+                    'name' => 'placeholder0001'
+                ],
+                [
+                    'sku' => 'ref_0002',
+                    'name' => 'placeholder0002'
+                ]
+            ]
+        ]);
     }
 }
